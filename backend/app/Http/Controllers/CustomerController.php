615f6beb2e621d0148fd91fb0049cbfb20f5cb0a
Ardini Andrea <ardini.a@gmail.com>
2025-10-03 09:40:04 +0200

    Creati i metodi index, show e customerWithOrder in CustomerController. Create le rotte api per i metodi. Iniziato a lavorare sul frontend creando il service customer.

--- backend/app/Http/Controllers/CustomerController.php
+++ backend/app/Http/Controllers/CustomerController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-    //
+    // Lettura di tutti i Customer
+    public function index()
+    {
+        return response()->json(Customer::all(), 200);
+    }
+
+    // Lettura di un determinato Customer
+    public function show(Customer $customer)
+    {
+        return response()->json($customer, 200);
+    }
+
+    // Ritorna un customer specifico con i suoi ordini
+    public function customerWithOrder(Customer $customer)
+    {
+        $orders = $customer->orders;
+        return response()->json($orders, 200);
+        // return $customer->load('orders');
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -7,6 +8,9 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/test', function () {
-    return response()->json(['message' => '✅ Connessione da Laravel OK!']);
-});
+// Route::get('/test', function () {
+//     return response()->json(['message' => '✅ Connessione da Laravel OK!']);
+// });
+
+Route::apiResource('customers', CustomerController::class);
+Route::get('/customers/{customer}', [CustomerController::class], 'customerWithOrder');
