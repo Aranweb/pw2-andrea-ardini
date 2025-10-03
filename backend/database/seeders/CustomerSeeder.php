a8b963091a7ef125fc25e3e82f2e827b4db8aa82
Ardini Andrea <ardini.a@gmail.com>
2025-10-03 09:18:23 +0200

    Creati e popolati i seeder. Fatta la migrazione al db

--- backend/database/seeders/CustomerSeeder.php
+++ backend/database/seeders/CustomerSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Customer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,29 @@ class CustomerSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Customer::create([
+            'name' => 'Mario Rossi',
+            'image' => 'customers/customer-1-large.jpg',
+            'address' => 'Viale Roma 1, 47924 Rimini',
+            'email' => 'mario.rossi@info.it',
+        ]);
+        Customer::create([
+            'name' => 'Giuseppe Verdi',
+            'image' => 'customers/customer-2-large.jpg',
+            'address' => 'Via Italia 3, 20019 Milano',
+            'email' => 'giuseppe.verdi@info.it',
+        ]);
+        Customer::create([
+            'name' => 'Chiara Bianchi',
+            'image' => 'customers/customer-3-large.jpg',
+            'address' => 'Largo Piemonte 5, 00100 Roma',
+            'email' => 'chiara.bianchi@info.it',
+        ]);
+        Customer::create([
+            'name' => 'Laura Neri',
+            'image' => 'customers/customer-4-large.jpg',
+            'address' => 'Piazza del popolo 7, 40100 Bologna',
+            'email' => 'laura.neri@info.it',
+        ]);
     }
 }
--- backend/database/seeders/OrderSeeder.php
+++ backend/database/seeders/OrderSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Order;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,47 @@ class OrderSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Order::create([
+            'customer_id' => 3,
+            'order_number' => 'OC0001',
+            'description' => 'Camicia da donna bianca taglia M',
+            'order_data' => '2025-07-10',
+            'total' => 69.99,
+        ]);
+        Order::create([
+            'customer_id' => 1,
+            'order_number' => 'OC0002',
+            'description' => 'Pantalone uomo di cotone nero taglia 50',
+            'order_data' => '2025-08-15',
+            'total' => 89.99,
+        ]);
+        Order::create([
+            'customer_id' => 4,
+            'order_number' => 'OC0003',
+            'description' => 'T-shirt donna manica lunga blu stampa teschio taglia M',
+            'order_data' => '2025-09-01',
+            'total' => 24.99,
+        ]);
+        Order::create([
+            'customer_id' => 2,
+            'order_number' => 'OC0004',
+            'description' => 'T-shirt uomo manica lunga verde stampa drago taglia XL',
+            'order_data' => '2025-06-28',
+            'total' => 24.99,
+        ]);
+        Order::create([
+            'customer_id' => 2,
+            'order_number' => 'OC0005',
+            'description' => 'Camicia uomo manica corta a righe azzurra/blu taglia 42',
+            'order_data' => '2025-05-09',
+            'total' => 49.99,
+        ]);
+        Order::create([
+            'customer_id' => 1,
+            'order_number' => 'OC0006',
+            'description' => 'Giacca elegante uomo, doppio petto nera taglia 52',
+            'order_data' => '2025-10-01',
+            'total' => 249.99,
+        ]);
     }
 }
