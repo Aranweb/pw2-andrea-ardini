6f13805359dcc874d94acb5d9163dafbd27215cd
Ardini Andrea <ardini.a@gmail.com>
2025-10-05 18:23:27 +0200

    Modificato favicon sia frontend che backend. Modificata pagina web del backend

---
+++ backend/resources/views/api-status.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <title>API pw2-andrea-ardini</title>
+    <link rel="stylesheet" href="{{ asset('css/api-status.css') }}">
+    <link rel="icon" type="image/x-icon" href="{{ asset('aa-favicon.png') }}"> </head>
+</head>
+<body>
+    <div>
+        <h1>✅ API is Running</h1>
+        <p>pw2-andrea-ardini Backend is operational.</p>
+    </div>
+</body>
+</html>
