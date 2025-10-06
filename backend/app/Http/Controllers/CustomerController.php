936e5f1346820b08ed5ee22f7d6ba9ff960b4443
Ardini Andrea <ardini.a@gmail.com>
2025-10-06 09:46:39 +0200

    Creato le funzioni CRUD per le entità clienti e ordini nel backend

--- backend/app/Http/Controllers/CustomerController.php
+++ backend/app/Http/Controllers/CustomerController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Customer;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Validated;
 
 class CustomerController extends Controller
 {
@@ -18,5 +19,54 @@ class CustomerController extends Controller
     {
         // Utilizzo load per caricare la relazione 'orders' dentro l'oggetto $customer
         return $customer->load('orders');
+    }
+
+    // Creazione entità Cliente
+    public function store(Request $request) {
+
+        // Validazione dei campi in creazione
+        $validated = $request->validate([
+            'name' => ['required', 'string'],
+            'image' => ['nullable', 'string'],
+            'address' => ['required', 'string'],
+            'email' => ['required', 'string', 'unique:customers,email'],
+        ],[
+            'name.required' => 'Il campo nome è obbligatorio',
+            'address.required' => 'Il campo indirizzo è obbligatorio',
+            'email.required' => 'Il campo mail è obbligatorio',
+            'email.unique' => 'Il campo mail è già presente in archivio',
+        ]);
+
+        $customer = Customer::create($validated);
+
+        return response()->json($customer, 201);
+    }
+
+    // Aggiornamento entità Cliente
+    public function update(Request $request, Customer $customer) {
+
+        // Validazione dei campi in aggiornamento
+        $validated = $request->validate([
+            'name' => ['sometimes', 'string'],
+            'image' => ['sometimes', 'string'],
+            'address' => ['sometimes', 'string'],
+            'email' => ['sometimes', 'string', 'unique:customers,email,'.$customer->id],
+        ],[
+            'name.required' => 'Il campo nome è obbligatorio',
+            'address.required' => 'Il campo indirizzo è obbligatorio',
+            'email.required' => 'Il campo mail è obbligatorio',
+            'email.unique' => 'Il campo mail è già presente in archivio',
+        ]);
+
+        $customer->update($validated);
+
+        return response()->json($customer, 200);
+    }
+
+    // Cancellazione entità Cliente
+    public function destroy(Customer $customer) {
+        $customer->delete();
+
+        return response()->json(null, 204);
     }
 }
--- backend/app/Http/Controllers/OrderController.php
+++ backend/app/Http/Controllers/OrderController.php
@@ -2,9 +2,75 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    //
+    // Le funzioni index e show non vengono create perchè l'entità ordini viene richiamata dal load dell'index dell'entità cliente.
+    // Nell'applicazione non vengono mai mostrati solo gli ordini ma sempre associati al cliente collegato.
+    
+    // Creazione entità Ordine
+    public function store(Request $request)
+    {
+        
+        // Validazione dei campi in creazione
+        $validated = $request->validate([
+            'customer_id'=>['required', 'integer', 'exists:customers,id'],
+            'order_number'=>['required', 'string', 'unique:orders,order_number', 'max:4'], // nel frontend costruisco poi l'ordine tipo OC0001
+            'description'=>['required', 'string'],
+            'order_data'=>['required', 'date'],
+            'total'=>['required', 'numeric', 'min:0'],
+        ], [
+            'customer_id.required' => 'Il codice cliente è obbligatorio',
+            'customer_id.exists' => 'Il codice cliente specificato non esiste',
+            'order_number.required'=> 'Il numero di ordine è obbligatorio',
+            'order_number.unique'=> 'Il numero di ordine inserito esiste già',
+            'order_number.max'=> 'Il numero di ordine non può essere più lungo di 4 caratteri',
+            'description.required'=> 'La descrizione è obbligatoria',
+            'order_data.required'=> 'La data ordine è obbligatoria',
+            'total.required'=> 'Il totale ordine è obbligatorio',
+            'total.min'=> 'Il totale ordine non può essere inferiore a zero',
+        ]);
+        
+        $order = Order::create($validated);
+        
+        return response()->json($order, 201);
+    }
+    
+    // Aggiornamento entità Ordine
+    public function update(Request $request, Order $order)
+    {
+        
+        // Validazione dei campi in aggiornamento
+        $validated = $request->validate([
+            'customer_id'=>['sometimes', 'integer', 'exists:customers,id'],
+            'order_number'=>['sometimes', 'string', 'unique:orders,order_number,'.$order->id, 'max:4'], 
+            'description'=>['sometimes', 'string'],
+            'order_data'=>['sometimes', 'date'],
+            'total'=>['sometimes', 'numeric', 'min:0'],
+        ], [
+            'customer_id.required' => 'Il codice cliente è obbligatorio',
+            'customer_id.exists' => 'Il codice cliente specificato non esiste',
+            'order_number.required'=> 'Il numero di ordine è obbligatorio',
+            'order_number.unique'=> 'Il numero di ordine inserito esiste già',
+            'order_number.max'=> 'Il numero di ordine non può essere più lungo di 4 caratteri',
+            'description.required'=> 'La descrizione è obbligatoria',
+            'order_data.required'=> 'La data ordine è obbligatoria',
+            'total.required'=> 'Il totale ordine è obbligatorio',
+            'total.min'=> 'Il totale ordine non può essere inferiore a zero',
+        ]);
+
+        $order->update($validated);
+
+        return response()->json($order, 200);
+    }
+
+    // Cancellazione entità Ordine
+    public function destroy(Order $order)
+    {
+        $order->delete();
+
+        return response()->json(null, 204);
+    }
 }
